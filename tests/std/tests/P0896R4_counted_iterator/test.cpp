7cabc0efe57a7aaaa4c6e77aa34a3ebc3e16a1fb
Yehezkel Bernat <YehezkelShB@gmail.com>
2023-01-01 19:31:27 +0200

    Remove irrelevant test changes and have tests for lazy_counted_iterator - STILL FAILS

--- tests/std/tests/P0896R4_counted_iterator/test.cpp
+++ tests/std/tests/P0896R4_counted_iterator/test.cpp
@@ -7,6 +7,7 @@
 #include <list>
 #include <memory>
 #include <type_traits>
+#include <streambuf>
 
 #include <range_algorithm_support.hpp>
 using namespace std;
@@ -306,6 +307,271 @@ struct instantiator {
     }
 };
 
+struct lazy_instantiator {
+    template <input_or_output_iterator Iter>
+    static constexpr void call() {
+        using ConstIter = typename Iter::Consterator;
+
+        int input[5] = {1, 2, 3, 4, 5};
+        // [lazy.counted.iter.const]
+        {
+            STATIC_ASSERT(default_initializable<lazy_counted_iterator<Iter>> == default_initializable<Iter>);
+            if constexpr (default_initializable<Iter>) {
+                [[maybe_unused]] lazy_counted_iterator<Iter> defaultConstructed{};
+            }
+
+            lazy_counted_iterator<Iter> constructed(Iter{input}, iter_difference_t<Iter>{2});
+            lazy_counted_iterator<Iter> constructedEmpty{Iter{input}, iter_difference_t<Iter>{0}};
+
+            if constexpr (copyable<Iter>) { // lazy_counted_iterator only has const lvalue conversions
+                lazy_counted_iterator<ConstIter> constructedConversion{constructed};
+                constructedConversion = constructedEmpty;
+            }
+        }
+
+        // [lazy.counted.iter.access]
+        {
+            if constexpr (copy_constructible<Iter>) {
+                const lazy_counted_iterator<Iter> lvalueBase{Iter{input}, 2};
+                if constexpr (random_access_iterator<Iter>) {
+                    const same_as<Iter> auto base1 = lvalueBase.base();
+                    assert(base1.peek() == input);
+                }
+            }
+
+            if constexpr (random_access_iterator<Iter>) {
+                const same_as<Iter> auto base2 = lazy_counted_iterator<Iter>{Iter{input}, 2}.base();
+                assert(base2.peek() == input);
+            }
+
+            const same_as<iter_difference_t<Iter>> auto length = lazy_counted_iterator<Iter>{Iter{input}, 2}.count();
+            assert(length == 2);
+        }
+
+        // [lazy.counted.iter.elem]
+        {
+            lazy_counted_iterator<Iter> iter{Iter{input}, 2};
+            assert(*iter == 1);
+            if constexpr (is_reference_v<iter_reference_t<Iter>>) {
+                assert(addressof(*iter) == input);
+            }
+
+            const lazy_counted_iterator<Iter> constIter{Iter{input}, 2};
+            assert(*constIter == 1);
+            if constexpr (is_reference_v<iter_reference_t<Iter>>) {
+                assert(addressof(*constIter) == input);
+            }
+
+            if constexpr (random_access_iterator<Iter>) {
+                assert(iter[1] == 2);
+                if constexpr (is_reference_v<iter_reference_t<Iter>>) {
+                    assert(addressof(iter[1]) == input + 1);
+                }
+            }
+        }
+        // [lazy.counted.iter.nav]
+        {
+            { // pre increment
+                lazy_counted_iterator<Iter> iter{Iter{input}, 2};
+                assert(addressof(++iter) == addressof(iter));
+                assert(iter.count() == 1);
+                assert(*iter == 2);
+                if constexpr (is_reference_v<iter_reference_t<Iter>>) {
+                    assert(addressof(*iter) == input + 1);
+                }
+            }
+
+            if constexpr (forward_iterator<Iter>) { // post increment
+                lazy_counted_iterator<Iter> iter{Iter{input}, 2};
+                auto ref_iter = iter;
+                auto iter2    = iter++;
+                assert(iter2 == ref_iter);
+                assert(iter.count() == 1);
+                assert(*iter == 2);
+                if constexpr (is_reference_v<iter_reference_t<Iter>>) {
+                    assert(addressof(*iter) == input + 1);
+                }
+            }
+
+            if constexpr (bidirectional_iterator<Iter>) {
+                {
+                    // pre decrement
+                    lazy_counted_iterator<Iter> iter{Iter{input + 2}, 3};
+                    assert(addressof(--iter) == addressof(iter));
+                    assert(iter.count() == 4);
+                    assert(*iter == 2);
+                    if constexpr (is_reference_v<iter_reference_t<Iter>>) {
+                        assert(addressof(*iter) == input + 1);
+                    }
+                }
+                { // post decrement
+                    lazy_counted_iterator<Iter> iter{Iter{begin(input) + 2}, 3};
+                    auto ref_iter = iter;
+                    auto iter2    = iter--;
+                    assert(iter2 == ref_iter);
+                    assert(iter.count() == 4);
+                    assert(*iter == 2);
+                    if constexpr (is_reference_v<iter_reference_t<Iter>>) {
+                        assert(addressof(*iter) == input + 1);
+                    }
+                }
+            }
+
+            if constexpr (random_access_iterator<Iter>) {
+                { // increment by n lhs
+                    lazy_counted_iterator<Iter> iter{Iter{input}, 5};
+                    auto ref_iter = iter + 3;
+                    assert(iter.count() == 5);
+                    assert(*iter == 1);
+                    assert(iter.count() == ref_iter.count() + 3);
+                    assert(iter.base().peek() == prev(ref_iter.base().peek(), 3));
+                }
+                { // increment by n rhs
+                    lazy_counted_iterator<Iter> iter{Iter{input}, 5};
+                    auto ref_iter = 3 + iter;
+                    assert(iter.count() == 5);
+                    assert(*iter == 1);
+                    assert(iter.count() == ref_iter.count() + 3);
+                    assert(iter.base().peek() == prev(ref_iter.base().peek(), 3));
+                }
+                { // increment value-initialized by 0
+                    lazy_counted_iterator<Iter> iter;
+                    auto ref_iter = 0 + iter;
+                    assert(iter == ref_iter);
+                }
+                { // increment assign
+                    lazy_counted_iterator<Iter> iter{Iter{input}, 5};
+                    auto ref_iter = iter;
+                    assert(addressof(iter += 3) == addressof(iter));
+                    assert(iter.count() == 2);
+                    assert(*iter == 4);
+                    assert(iter.count() == ref_iter.count() - 3);
+                    assert(iter.base().peek() == next(ref_iter.base().peek(), 3));
+                }
+                { // decrement by n
+                    lazy_counted_iterator<Iter> iter{Iter{begin(input) + 2}, 3};
+                    auto ref_iter = iter - 2;
+                    assert(iter.count() == 3);
+                    assert(*iter == 3);
+                    assert(iter.count() == ref_iter.count() - 2);
+                    assert(iter.base().peek() == next(ref_iter.base().peek(), 2));
+                }
+            }
+            { // difference
+                lazy_counted_iterator<Iter> iter1{Iter{input + 1}, 2};
+                lazy_counted_iterator<Iter> iter2{Iter{input}, 3};
+
+                const same_as<iter_difference_t<Iter>> auto diff1 = iter1 - iter2;
+                assert(diff1 == 1);
+
+                const same_as<iter_difference_t<Iter>> auto diff2 = iter2 - iter1;
+                assert(diff2 == -1);
+            }
+            if constexpr (default_initializable<Iter>) { // difference value-initialized
+                const same_as<iter_difference_t<Iter>> auto diff1 = lazy_counted_iterator<Iter>{} - lazy_counted_iterator<Iter>{};
+                assert(diff1 == 0);
+            }
+            STATIC_ASSERT(CountedCompare<Iter, ConstIter> == common_with<Iter, ConstIter>);
+            STATIC_ASSERT(CountedCompare<ConstIter, Iter> == common_with<Iter, ConstIter>);
+
+            if constexpr (common_with<Iter, ConstIter>) { // cross-type difference
+                lazy_counted_iterator<Iter> iter1{Iter{input + 1}, 2};
+                lazy_counted_iterator<ConstIter> iter2{ConstIter{input}, 3};
+
+                const same_as<iter_difference_t<Iter>> auto diff1 = iter1 - iter2;
+                assert(diff1 == 1);
+
+                const same_as<iter_difference_t<ConstIter>> auto diff2 = iter2 - iter1;
+                assert(diff2 == -1);
+            }
+
+            { // difference default sentinel
+                lazy_counted_iterator<Iter> iter1{Iter{input}, 2};
+
+                const same_as<iter_difference_t<Iter>> auto diff1 = iter1 - default_sentinel;
+                assert(diff1 == -2);
+
+                const same_as<iter_difference_t<Iter>> auto diff2 = default_sentinel - iter1;
+                assert(diff2 == 2);
+            }
+
+            if constexpr (random_access_iterator<Iter>) { // decrement assign
+                lazy_counted_iterator<Iter> iter{Iter{end(input)}, 0};
+                auto ref_iter = iter;
+                assert(addressof(iter -= 3) == addressof(iter));
+                assert(iter.count() == 3);
+                assert(*iter == 3);
+                assert(iter.count() == ref_iter.count() + 3);
+                assert(iter.base().peek() == prev(ref_iter.base().peek(), 3));
+            }
+        }
+        // [lazy.counted.iter.cmp]
+        {
+            { // equality
+                lazy_counted_iterator<Iter> iter1{Iter{input}, 2};
+                lazy_counted_iterator<Iter> iter2{Iter{input}, 2};
+                lazy_counted_iterator<Iter> iter3{Iter{input + 1}, 1};
+                assert(iter1 == iter2);
+                assert(!(iter1 == iter3));
+            }
+            { // inequality
+                lazy_counted_iterator<Iter> iter1{Iter{input + 1}, 2};
+                lazy_counted_iterator<Iter> iter2{Iter{input}, 3};
+                lazy_counted_iterator<Iter> iter3{Iter{input}, 3};
+                assert(iter1 != iter2);
+                assert(!(iter2 != iter3));
+            }
+            { // equality default sentinel
+                lazy_counted_iterator<Iter> iter1{Iter{input}, 2};
+                lazy_counted_iterator<Iter> iter2{Iter{input}, 0};
+                assert(iter1 != default_sentinel);
+                assert(default_sentinel != iter1);
+                assert(iter2 == default_sentinel);
+                assert(default_sentinel == iter2);
+            }
+            { // spaceship
+                lazy_counted_iterator<Iter> iter1{Iter{input}, 2};
+                lazy_counted_iterator<Iter> iter2{Iter{input + 2}, 0};
+                const same_as<strong_ordering> auto result = iter1 <=> iter2;
+                assert(result == strong_ordering::less);
+                assert(iter2 <=> iter1 == strong_ordering::greater);
+                assert(iter1 <=> iter1 == strong_ordering::equal);
+                assert(iter1 <=> iter1 == strong_ordering::equivalent);
+            }
+            if constexpr (default_initializable<Iter>) { // spaceship value-initialized
+                assert(lazy_counted_iterator<Iter>{} <=> lazy_counted_iterator<Iter>{} == strong_ordering::equal);
+                assert(lazy_counted_iterator<Iter>{} <=> lazy_counted_iterator<Iter>{} == strong_ordering::equivalent);
+            }
+
+            if constexpr (common_with<Iter, ConstIter>) {
+                { // equality converting
+                    lazy_counted_iterator<ConstIter> const_iter1{ConstIter{input}, 2};
+                    lazy_counted_iterator<Iter> iter2{Iter{input}, 2};
+                    lazy_counted_iterator<Iter> iter3{Iter{input + 1}, 1};
+                    assert(const_iter1 == iter2);
+                    assert(!(const_iter1 == iter3));
+                }
+                { // inequality converting
+                    lazy_counted_iterator<Iter> iter1{Iter{input + 1}, 2};
+                    lazy_counted_iterator<ConstIter> const_iter1{ConstIter{input}, 3};
+                    lazy_counted_iterator<Iter> iter3{Iter{input}, 3};
+                    assert(iter1 != const_iter1);
+                    assert(!(const_iter1 != iter3));
+                }
+                { // spaceship converting
+                    lazy_counted_iterator<Iter> iter{Iter{input}, 2};
+                    lazy_counted_iterator<ConstIter> const_iter{ConstIter{input + 2}, 0};
+                    const same_as<strong_ordering> auto result = iter <=> const_iter;
+                    assert(result == strong_ordering::less);
+                    assert(const_iter <=> iter == strong_ordering::greater);
+                    assert(iter <=> iter == strong_ordering::equal);
+                    assert(iter <=> iter == strong_ordering::equivalent);
+                }
+            }
+        }
+    }
+};
+
 // Also test P2259R1 Repairing input range adaptors and counted_iterator
 struct simple_forward_iter {
     using value_type        = double;
@@ -343,9 +609,24 @@ void test_P2259() {
     assert(ri->m == 1);
 }
 
+void lazy_test_P2259() {
+    struct A {
+        int m;
+    };
+    A a[2] = {{1}, {2}};
+    lazy_counted_iterator ci{a, 2};
+    reverse_iterator ri{ci + 1};
+    static_assert(contiguous_iterator<decltype(ci)>);
+    assert(ci->m == 1);
+    assert(ri->m == 1);
+}
+
 int main() {
     STATIC_ASSERT(with_writable_iterators<instantiator, int>::call());
     with_writable_iterators<instantiator, int>::call();
+
+    STATIC_ASSERT(with_writable_iterators<lazy_instantiator, int>::call());
+    with_writable_iterators<lazy_instantiator, int>::call();
 
     { // Validate unwrapping
         list<int> lst{0, 1, 2};
@@ -356,7 +637,17 @@ int main() {
         assert((ci == counted_iterator{ranges::next(lst.begin()), 1}));
     }
 
+    { // Validate unwrapping
+        list<int> lst{0, 1, 2};
+        lazy_counted_iterator ci{lst.begin(), 2};
+        same_as<lazy_counted_iterator<_Unwrapped_t<list<int>::iterator>>> auto uci = _Get_unwrapped(ci);
+        ++uci;
+        _Seek_wrapped(ci, uci);
+        assert((ci == lazy_counted_iterator{ranges::next(lst.begin()), 1}));
+    }
+
     test_P2259();
+    lazy_test_P2259();
 }
 
 // Tests for P2406R2 - Add lazy_counted_iterator
@@ -364,8 +655,6 @@ template <typename I>
 concept CanInstantiateLazyCountedIterator = requires { typename counted_iterator<I>; };
 
 using LCI = lazy_counted_iterator<simple_forward_iter>;
-using random_access_iter = std::vector<double>::iterator;
-using LCI_RA = lazy_counted_iterator<random_access_iter>;
 
 static_assert(same_as<iterator_traits<simple_forward_iter>::iterator_category, input_iterator_tag>);
 static_assert(forward_iterator<simple_forward_iter>);
@@ -375,13 +664,3 @@ static_assert(same_as<LCI::value_type, double>);
 static_assert(same_as<LCI::difference_type, long>);
 static_assert(same_as<LCI::iterator_category, input_iterator_tag>);
 static_assert(same_as<LCI::iterator_concept, forward_iterator_tag>);
-
-static_assert(same_as<iterator_traits<random_access_iter>::iterator_category, random_access_iterator_tag>);
-static_assert(forward_iterator<random_access_iter>);
-static_assert(forward_iterator<LCI_RA>);
-static_assert(!contiguous_iterator<LCI_RA>);
-static_assert(same_as<LCI_RA::value_type, double>);
-static_assert(same_as<LCI_RA::difference_type, long>);
-static_assert(same_as<LCI_RA::iterator_category, input_iterator_tag>);
-static_assert(same_as<LCI_RA::iterator_concept, forward_iterator_tag>);
-static_assert(!same_as<LCI_RA::iterator_concept, random_access_iterator_tag>);
